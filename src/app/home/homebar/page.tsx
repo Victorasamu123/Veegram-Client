1efc258fe5f83b3123cf8aee381fa0a24007c1f5
Victorasamu123 <asamuvictor03@gmail.com>
2025-03-05 23:06:49 +0100

    proceeded story

--- src/app/home/homebar/page.tsx
+++ src/app/home/homebar/page.tsx
@@ -1,14 +1,12 @@
 "use client"
 import Image from 'next/image'
-import storyOne from "../../../../public/people-images/aiony-haust-3TLl_97HNJo-unsplash (1).jpg"
-import storyTwo from "../../../../public/people-images/aiony-haust-f2ar0ltTvaI-unsplash (1).jpg"
-import storyThree from "../../../../public/people-images/albert-dera-ILip77SbmOE-unsplash (1).jpg"
-import storyFour from "../../../../public/people-images/alex-perri-At__EKm5PGE-unsplash (1).jpg"
-import storyFive from "../../../../public/people-images/arvin-keynes-IPETsB4dcCs-unsplash (1).jpg"
-import storySix from "../../../../public/people-images/averie-woodard-WsSdUPXVb4M-unsplash (1).jpg"
-import storySeven from "../../../../public/people-images/bardia-hashemirad-3P7jxpEgkC4-unsplash (1).jpg"
-import storyEight from "../../../../public/people-images/jessica-felicio-nIKCy5CWIzk-unsplash (1).jpg"
-import storyNine from "../../../../public/people-images/aiony-haust-f2ar0ltTvaI-unsplash (1).jpg"
+import storyOne from "../../../../public/people-images/jessica-felicio-nIKCy5CWIzk-unsplash (1).jpg"
+import storyTwo from "../../../../public/people-images/caroline-hernandez-5BKUtjC7O6A-unsplash (1).jpg"
+import storyThree from "../../../../public/people-images/caleb-ekeroth-wSBQFWF77lI-unsplash (1).jpg"
+import storyFour from "../../../../public/people-images/caique-nascimento-C5NOq1BKlAk-unsplash (1).jpg"
+import storyFive from "../../../../public/people-images/brooke-cagle-emlKHDEydhg-unsplash (1).jpg"
+import storySix from "../../../../public/people-images/brooke-cagle-TK35M7fUWqM-unsplash (1).jpg"
+import storySeven from "../../../../public/people-images/brooke-cagle-R0Ea06wC2IM-unsplash (1).jpg"
 import "./persona.css"
 export default function HomeBar() {
   
@@ -17,26 +15,30 @@ export default function HomeBar() {
     {/* <div>gti</div> */}
       <div className='bg-[#E8E8E8] w-[55%] h-full mt-4'>
         {/* storeis */}
-      <div className='pl-8 pt-[35px] pr-8 flex pb-2'>
-       <div className='w-[100px] h-[130px] rounded-xl p-[2px] backgrounding'>
+      <div className='pl-10 pt-[35px] pr-8 flex pb-[35px]'>
+       <div className='w-[100px] h-[140px] rounded-[7px] p-[2px] backgrounding'>
+         <p className='flex'>
+          <span className='w-[30px] border-black border-[2px] spanOne'>
+         
+          </span>
+         </p>
+       </div>
+       <div className='ml-4 w-[100px] h-[140px] rounded-[7px] p-[2px] backgroundingOne'>
         
        </div>
-       <div className='ml-4 w-[100px] h-[130px] rounded-xl p-[2px] backgroundingOne'>
+       <div className='ml-4 w-[100px] h-[140px] rounded-[7px] p-[2px] backgroundingTwo'>
         
        </div>
-       <div className='ml-4 w-[100px] h-[130px] rounded-xl p-[2px] backgroundingTwo'>
+       <div className='ml-4 w-[100px] h-[140px] rounded-[7px] p-[2px] backgroundingThree'>
         
        </div>
-       <div className='ml-4 w-[100px] h-[130px] rounded-xl p-[2px] backgroundingThree'>
-        
-       </div>
-       <div className='ml-4 w-[100px] h-[130px] rounded-xl p-[2px] backgroundingFour'>
+       <div className='ml-4 w-[100px] h-[140px] rounded-[7px] p-[2px] backgroundingFour'>
        
        </div>
-       <div className='ml-4 w-[100px] h-[130px] rounded-xl p-[2px] backgroundingFive'>
+       <div className='ml-4 w-[100px] h-[140px] rounded-[7px] p-[2px] backgroundingFive'>
         
        </div>
-       <div className='ml-4 w-[100px] h-[130px] rounded-xl p-[2px] backgroundingSix'>
+       <div className='ml-4 w-[100px] h-[140px] rounded-[7px] p-[2px] backgroundingSix'>
       
        </div>
       </div>
